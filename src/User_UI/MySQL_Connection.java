a36fc4e56969183c92a8304607226d4e2273943d
jomardemartin <ocampomark61@yahoo.com>
2021-07-30 17:24:05 +0800

    initial commit

---
+++ src/User_UI/MySQL_Connection.java
@@ -0,0 +1,25 @@
+package User_UI;
+
+import Login_Registration.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author John Mark
+ */
+public class MySQL_Connection {
+        public static Connection getConnection(){
+        
+        Connection conn = null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/attendancesystem", "root","root");           
+        }
+        catch(Exception ex){  
+        }
+        return conn;
+    }
+    
+}
